5b6e1c33730e8be1bd3ad73bcee245f6249be2c7
Natanael Teixeira <natanaelteixeiradejesus2@example.com>
2024-08-30 01:50:27 -0300

    Projeto palindromo

---
+++ Exercicios/ExsFunction/palindromo.js
@@ -0,0 +1,15 @@
+function isPalindrome(){
+    let reverse = ""
+    let arguments[0] = prompt("insira uma palavra:")
+    for (let i = 0; i < array.length; i++) {
+        const element = array[index];     
+    }
+    if(reverse === arguments[0]){
+        return console.log(true)
+    } else{
+        return console.log(false)
+    }
+
+}
+
+isPalindrome()
