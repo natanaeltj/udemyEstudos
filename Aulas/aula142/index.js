115614c21231faf2743f503069398d905f9af3f2
Natanael Teixeira <natanaelteixeiradejesus2@gmail.com>
2024-12-22 06:18:46 -0300

    objetos

---
+++ Aulas/aula142/index.js
@@ -0,0 +1,14 @@
+let objetoA = {
+    nome:"joca",
+    idade:26,
+}
+
+let objetoB = {
+    balde:"luis",
+    cm:"46",
+}
+
+Object.assign(objetoA, objetoB);
+
+console.log(objetoA);
+prompt('sla')
